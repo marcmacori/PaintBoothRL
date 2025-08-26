64daca051151cf847267b2d4bb2fec4f0895e490
marcmacori <marcmacoril@iqs.url.edu>
2025-08-26 20:37:30 +0200

    another fresh start... no vibe coding this time.

--- oven_batching/agents/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Agents module for Dynamic Oven Batching Environment
-"""
-
-from .random_agent import RandomAgent
-from .ppo_agent import BasicPPOAgent
-
-__all__ = ["RandomAgent", "BasicPPOAgent"]
--- oven_batching/environment/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Dynamic Oven Batching Environment Package
-"""
-
-from .core import DynamicOvenBatchingEnv, Oven, Job, OvenStatus
-
-__version__ = "1.0.0"
-__all__ = ["DynamicOvenBatchingEnv", "Oven", "Job", "OvenStatus"]
